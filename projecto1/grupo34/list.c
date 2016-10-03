13a0b4f05a6738d9738fe4e5ebedd4d801c032c3
Miguel <libraries.area@gmail.com>
2016-10-03 18:31:03 +0100

    some changes

--- projecto1/grupo34/list.c
+++ projecto1/grupo34/list.c
@@ -1,9 +1,9 @@
 /* TODO PUT HEADER HERE */
 
+
 /* Cria uma nova lista. Em caso de erro, retorna NULL.
  */
 struct list_t *list_create() {
-	//PARA COMPLETAR
 	struct list_t *l;
 	list_t -> head = NULL;
 	return head;
@@ -13,7 +13,12 @@ struct list_t *list_create() {
  * lista.
  */
 void list_destroy(struct list_t *list) {
+  
+}
 
+/* novo metodo para fazer sort a lista*/
+void sort() {
+	// TODO
 }
 
 /* Adiciona uma entry na lista. Como a lista deve ser ordenada, 
@@ -26,8 +31,14 @@ int list_add(struct list_t *list, struct entry_t *entry) {
 	struct node_t *newNode = (struct node_t*) malloc(sizeof(struct node_t));
 
 	newNode -> entry_t = entry;
-	newNode -
-	}
+	
+	// point it to the old first node
+	newNode -> next = list -> head;
+	
+	// point first to the new first
+	list_t -> head = newNode;
+	list_t -> size = list_t -> size + 1;
+	sort(list_t *list);
 }
 
 /* Elimina da lista um elemento com a chave key. 
@@ -35,31 +46,77 @@ int list_add(struct list_t *list, struct entry_t *entry) {
  */
 int list_remove(struct list_t *list, char* key) {
 
-}
+	struct node* current = list_t -> head;
+	struct node* previous = NULL;~
+	
+	// se a lista estiver vazia
+	if (list -> head == NULL) return -1;
+
+	while(current->key != key) {
+		if (current-> next == NULL) {
+			return -1;
+		}else{
+			previous = current;
+			current = current -> next;
+	    }
+	}
+	// encontrou
+	if (current == list -> head  {
+		(list -> head) = (list -> head -> next);
+
+	}else{
+		previous -> next = current -> next;
+	list_t -> size = list_t -> size - 1
+
+
 
 /* Obtem um elemento da lista que corresponda à chave key. 
  * Retorna a referência do elemento na lista (ou seja, uma alteração
  * implica alterar o elemento na lista). 
  */
 struct entry_t *list_get(struct list_t *list, char *key) {
+	struct node* current = list -> head;
 
+	if (list -> head == NULL) return NULL;
+
+	// correr a lista
+	while (current -> key != key) {
+		//se for o ultimo node
+		if (current -> next == NULL) {
+			return NULL;
+		}else{
+			current = current -> next
+		}
+	}
+	return current;
 }
 
 /* Retorna o tamanho (numero de elementos) da lista 
  * Retorna -1 em caso de erro.  */
 int list_size(struct list_t *list) {
-
+	return list_t -> size;
 }
 
 /* Devolve um array de char * com a cópia de todas as keys da 
  * tabela, e um último elemento a NULL.
  */
 char **list_get_keys(struct list_t *list) {
-
+	// criar array com o size da lista
+	// para cada elemento da lista:
+	// array[i] node -> entry -> key )
+	
+	struct node* current = list_t -> head;
+	**char list_keys[list_t -> size]; 
+	int i;
+	
+	for (i = 0; i < list_t -> size, i++) {
+		list_keys[i] = node -> entry -> key;
+		current = current -> next;
+	}
 }
 
 /* Liberta a memoria reservada por list_get_keys.
  */
 void list_free_keys(char **keys) {
-	
+	//TODO
 }
