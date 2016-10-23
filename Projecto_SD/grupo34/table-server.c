3e011a5a84a67502ffc90ea0ed102e3a8827855a
Silvia Ferreira <spadferreira@gmail.com>
2016-10-23 20:08:24 +0100

    acrescentei erro no message-private.h, o table-server ainda n esta acabado

--- Projecto_SD/grupo34/table-server.c
+++ Projecto_SD/grupo34/table-server.c
@@ -22,7 +22,7 @@ int make_server_socket(short port){
   }
 
   server.sin_family = AF_INET;
-  server.sin_port = htons(port);  
+  server.sin_port = htons(port);
   server.sin_addr.s_addr = htonl(INADDR_ANY);
 
   if (bind(socket_fd, (struct sockaddr *) &server, sizeof(server)) < 0){
@@ -46,14 +46,45 @@ int make_server_socket(short port){
 */
 struct message_t *process_message(struct message_t *msg_pedido, struct table_t *tabela){
 	struct message_t *msg_resposta;
-	
+  int result;
+
 	/* Verificar parâmetros de entrada */
+  if (msg_pedido == NULL || tabela == NULL) return NULL;
+
+  msg_resposta = (struct message_t *) malloc(sizeof(struct message_t));
 
 	/* Verificar opcode e c_type na mensagem de pedido */
+  /* Aplicar operação na tabela */
+  /* Preparar mensagem de resposta */
+  switch (msg_pedido->opcode) {
+    case OC_SIZE:
+      msg_resposta->opcode = OC_SIZE + 1;
+      msg_resposta->c_type = CT_RESULT;
+      msg_resposta->content.result = table_size(tabela);
+    break;
+    case OC_DEL:
+      result = table_del(tabela, msg_pedido->content.key);
+      if (result == -1) {
+        msg_resposta->opcode = OC_RT_ERROR;
+        msg_resposta->c_type = CT_RESULT;
+      } else {
+        msg_resposta->opcode = OC_DEL + 1;
+        msg_resposta->c_type = CT_RESULT
+      }
+      msg_resposta->conten.result = result;
+    break;
+    case OC_UPDATE:
+      //TODO
+    break;
+    case OC_GET:
+      //TODO
+    break;
+    case OC_PUT:
+      //TODO
+    break;
+  }
 
-	/* Aplicar operação na tabela */
 
-	/* Preparar mensagem de resposta */
 
 	return msg_resposta;
 }
@@ -75,24 +106,30 @@ int network_receive_send(int sockfd, struct table_t *table){
 
 	/* Verificar parâmetros de entrada */
 
-	/* Com a função read_all, receber num inteiro o tamanho da 
+	/* Com a função read_all, receber num inteiro o tamanho da
 	   mensagem de pedido que será recebida de seguida.*/
 	result = read_all(sockfd, (char *) &msg_size, _INT);
 
 	/* Verificar se a receção teve sucesso */
+  if (result == -1) return -1;
 
 	/* Alocar memória para receber o número de bytes da
 	   mensagem de pedido. */
+  message_pedido = malloc(sizeof(char)*msg_size);
 
 	/* Com a função read_all, receber a mensagem de resposta. */
-	result = read_all(sockfd, message_pedido, /* tamanho da mensagem */);
+	result = read_all(sockfd, message_pedido, msg_size);
 
 	/* Verificar se a receção teve sucesso */
+  //TODO libertar memoria em caso de erro
+  if (result == -1) return -1;
 
 	/* Desserializar a mensagem do pedido */
-	msg_pedido = buffer_to_message(message_pedido, /* tamanho da mensagem */);
+	msg_pedido = buffer_to_message(message_pedido, msg_size);
 
 	/* Verificar se a desserialização teve sucesso */
+  //TODO libertar memoria em caso de erro
+  if (msg_pedido == NULL) return NULL;
 
 	/* Processar a mensagem */
 	msg_resposta = process_message(msg_pedido, table);
@@ -101,22 +138,33 @@ int network_receive_send(int sockfd, struct table_t *table){
 	message_size = message_to_buffer(msg_resposta, &message_resposta);
 
 	/* Verificar se a serialização teve sucesso */
+  //TODO libertar memoria em caso de erro
+  if (message_size == -1) return -1;
 
 	/* Enviar ao cliente o tamanho da mensagem que será enviada
 	   logo de seguida
 	*/
 	msg_size = htonl(message_size);
+  //TODO DUVIDA ponteiro 'server' vem de onde?
  	result = write_all(server->/*atributo*/, (char *) &msg_size, _INT));
 
 	/* Verificar se o envio teve sucesso */
+  //TODO libertar memoria em caso de erro
+  if (result == -1) return -1;
 
 	/* Enviar a mensagem que foi previamente serializada */
 
 	result = write_all(server->/*atributo*/, message_resposta, message_size));
 
 	/* Verificar se o envio teve sucesso */
+  //TODO libertar memoria em caso de erro
+  if (result == -1) return -1;
 
 	/* Libertar memória */
+  free(message_pedido);
+  free(message_resposta);
+  free_message(msg_pedido); //message.h
+  free_message(msg_resposta); //message.h
 
 	return 0;
 }
@@ -135,7 +183,7 @@ int main(int argc, char **argv){
 	return -1;
 	}
 
-	if ((listening_socket = make_server(atoi(argv[1]))) < 0) return -1;
+	if ((listening_socket = make_server_socket(atoi(argv[1]))) < 0) return -1;
 
 	if ((table = table_create(atoi(argv[2]))) == NULL){
 	result = close(listening_socket);
