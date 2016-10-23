447b6af6ebab1d90c5c6ecc07cea53f2e9bf0590
Silvia Ferreira <spadferreira@gmail.com>
2016-10-23 18:33:29 +0100

    buffer_to_message

--- Projecto_SD/grupo34/message.c
+++ Projecto_SD/grupo34/message.c
@@ -54,12 +54,14 @@ struct message_t *buffer_to_message(char *msg_buf, int msg_size){
   /* Verificar se msg_buf é NULL */
   if (msg_buf == NULL) return NULL;
 
+  //TODO - msg_size < 7?????
   /* msg_size tem tamanho mínimo ? */
   if(msg_size < 7) return NULL;
 
   /* Alocar memória para uma struct message_t */
   struct message_t* msg;
   msg = (struct message_t *) malloc(sizeof(struct message_t));
+  if(msg == NULL) return NULL;
   /* Recuperar o opcode e c_type */
   short short_aux;
   //short_aux = ntohs(msg->opcode);
@@ -77,60 +79,83 @@ struct message_t *buffer_to_message(char *msg_buf, int msg_size){
   msg_buf += _SHORT;
   */
   /* O opcode e c_type são válidos? */
-  if(!isValidOC(msg->opcode) || !isValidCTC (msg->c_type)) return NULL;
-
+  if(!isValidOC(msg->opcode) || !isValidCTC(msg->c_type)) return NULL;
+  int int_aux;
   /* Consoante o c_type, continuar a recuperação da mensagem original */
-  switch (msg->c_type) {
-    int int_aux;
+  switch (msg->content) {
     case CT_RESULT:
+      memcpy(&int_aux, msg_buf, _INT);
+      msg->content.result = ntohs(int_aux);
     break;
     case CT_VALUE:
-      struct data_t* value;
+      struct data_t* data;
+      //datasize
       memcpy(&int_aux, msg_buf,_INT);
-      msg->c_type = ntohs(int_aux);
       msg_buf += _INT;
-      //como traduz da network p host???
-      value = data_create2(int_aux, msg_buf);
-      msg->data = value;
+      //data
+      data = data_create2(int_aux, msg_buf);
+      if(data == NULL) return NULL;
+      //TODO DUVIDA ntohost
+      msg->content.data = data;
       break;
     case CT_KEY:
-      char *key;
+      char *k;
+      //keysize
       memcpy(&short_aux, msg_buf,_SHORT);
-      msg->c_type = ntohs(short_aux);
       msg_buf += _SHORT;
-      //como traduz p host???
-      key = (char*) malloc(sizeof(char)*short_aux);
-      strcpy(key, msg_buf, short_aux);
-      msg->key = key;
+      //key
+      k = malloc(sizeof(char)*short_aux);
+      if(k == NULL) return NULL;
+      strcpy(k, msg_buf, short_aux);
+      //TODO DUVIDA ntohost
+      msg->content.key = K;
       break;
     case CT_KEYS:
       char** keys;
+      //nkeys
       memcpy(&int_aux, msg_buf,_INT);
-      msg->c_type = ntohs(int_aux);
-      //msg_buf += _INT;
-//como traduz
+      msg_buf += _INT;
       keys = (char**) malloc(sizeof(char)*int_aux);
+      if(keys == NULL) return NULL;
       int i;
       for(i = 0; i <int_aux; i++){
+        //keysize
+        memcpy(&short_aux, msg_buf,_SHORT);
         msg_buf += _SHORT;
-        memcpy(&short_aux, msg_buf,_INT);
-        short key_size;
-        key_size = short_aux;
-        msg_buf += _SHORT;
+        //key
         keys[i] = strndup(msg_buf, ntohs(short_aux));
-        msg_buf +=_SHORT;
+        if(keys[i] == NULL) return NULL;
+        msg_buf += short_aux;
       }
-      msg->keys = keys;
+      msg->content.keys = keys;
       break;
-    default:
+    case CT_ENTRY:
       struct entry_t* entry;
-      struct data* value;
+      struct data_t* data;
+      char*key;
+      //keysize
+      memcpy(&short_aux, msg_buf,_SHORT);
+      msg_buf += _SHORT;
+      //key
+      key = (char*)malloc(sizeof(char)*short_aux);
+      if(key == NULL) return NULL;
+      strcpy(key, msg_buf, short_aux);
+      //TODO DUVIDA - nthost
+      msg->content.key = key;
+      msg_buf += short_aux;
+      //datasize
       memcpy(&int_aux, msg_buf,_INT);
-      msg->c_type = ntohs(int_aux);
       msg_buf += _INT;
-      //como traduz da network p host???
-      value = data_create2(int_aux, msg_buf);
-      msg->data = value;
+      //data
+      data = data_create2(int_aux, msg_buf);
+      if(data == NULL) return NULL;
+      //TODO DUVIDA - ntohost
+      msg->content.data = data;
+      //recuperar a entry
+      entry = entry_create(key, value);
+      if(entry == NULL) return NULL;
+      msg->content.entry = entry;
+      break;
   }
   return msg;
 }
