b93072a847e28a9f8c8f334ed203c29fcd286a05
Aqua1986 <ptpais86@gmail.com>
2016-10-24 14:40:00 +0100

    warning hash_key resolved

--- Projecto_SD/grupo34/table.c
+++ Projecto_SD/grupo34/table.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "table-private.h"
-#include "list-private.h"
+#include "include/table-private.h"
+#include "include/list-private.h"
 
 
 int key_hash(char *key, int l){
@@ -20,14 +20,16 @@ int key_hash(char *key, int l){
 //	for(; *key != '0'; key++)
 //		soma = *key + (soma << 5) - soma;
 
-	char *ch;
-	ch = strcpy(ch, key); // @pedro estahs a usar a variavel ch sem a inicializar ~ miguel
+	char *ch = NULL;
+
+	ch = strcpy(ch, key);
 	short chlength = strlen(ch);
 
 	int i, soma;
 	for (soma=0, i=0; i < chlength; i++)
 		soma += ch[i];
 
+	free(ch);
 	return soma % l;
 }
 
