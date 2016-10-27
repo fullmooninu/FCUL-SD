ed462ee96e13a12eb8130c475b06ff692599967b
miguel <libraries.area@gmail.com>
2016-10-27 05:52:20 +0100

    new test

--- projecto1/grupo34/test.bash
+++ projecto1/grupo34/test.bash
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+if ! hash cppcheck &> /dev/null; then
+  echo 'please install cppcheck: apt-get install cppcheck'
+fi
+if ! hash flawfinder &> /dev/null; then
+  echo 'please install flawfinder: apt-get install flawfinder'
+fi
 make clean
 make
-cppcheck .
+flawfinder -C .
+cppcheck ./include --enable=all .
 ./test_data
 ./test_entry
 ./test_list
+
