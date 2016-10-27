6a8cdcf9da9d9e530132c902aaaa8a87a83bdc8b
miguel <libraries.area@gmail.com>
2016-10-27 21:26:39 +0100

    comment and ident

--- Projecto_SD/grupo34/table.c
+++ Projecto_SD/grupo34/table.c
@@ -14,164 +14,190 @@ int key_hash(char *key, int l){
 			soma += (int) key[i];
 		}
 	}
-		else{
-			for(i = 0; i < 3; i++){
-				soma += (int) key[i];
-			}
-			for(i = key_len-1; i < key_len-4; i--){
-				soma += (int)key[i];
-			}
+	else{
+		for(i = 0; i < 3; i++){
+			soma += (int) key[i];
 		}
-		return soma % l;
-
+		for(i = key_len-1; i < key_len-4; i--){
+			soma += (int)key[i];
+		}
 	}
+	return soma % l;
 
-	struct table_t *table_create(int n) {
+}
 
-		struct table_t *new_table;
+/* Função para criar/inicializar uma nova tabela hash, com n
+* linhas(n = módulo da função hash)
+*/
+struct table_t *table_create(int n) {
+
+	struct table_t *new_table;
 
 		/* n tem valor válido? */
-		if (n < 1) return NULL;
+	if (n < 1) return NULL;
 
 		/* Alocar memória para struct table_t */
-		new_table =(struct table_t*) malloc(sizeof(struct table_t));
+	new_table =(struct table_t*) malloc(sizeof(struct table_t));
 
-		if(!new_table) return NULL;
+	if(!new_table) return NULL;
 
 		/* Alocar memória para array de listas com n entradas
 		que ficará referenciado na struct table_t alocada. */
-		if((new_table -> list = (struct list_t **) malloc(n * sizeof(struct list_t*))) == NULL)
-		{
-			free(new_table-> list);
-			return NULL;
-		}
+	if((new_table -> list = (struct list_t **) malloc(n * sizeof(struct list_t*))) == NULL)
+	{
+		free(new_table-> list);
+		return NULL;
+	}
 
 		/* Inicializar listas.*/
-		int i;
+	int i;
 
-		for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		new_table -> list[i] = NULL;
 
 		/* Inicializar atributos da tabela. */
-		new_table->size = n;
-		new_table->nElem = 0;
+	new_table->size = n;
+	new_table->nElem = 0;
 
-		return new_table;
-	}
+	return new_table;
+}
 
-	void table_destroy(struct table_t *table) {
+/* Libertar toda a memória ocupada por uma tabela.
+*/
+void table_destroy(struct table_t *table) {
 
-		if(table != NULL)
-		{
-			int i;
-			struct list_t *list;
+	if(table != NULL)
+	{
+		int i;
+		struct list_t *list;
 
 			/*   Libertar memória das listas.*/
-			for(i=0; i < table->size; i++)
-			{
-				list = table->list[i];
-				while(list != NULL){
-					list_destroy(list);
-				}
+		for(i=0; i < table->size; i++)
+		{
+			list = table->list[i];
+			while(list != NULL){
+				list_destroy(list);
 			}
+		}
 
 			/*Libertar memória da tabela. */
-			free(table-> list);
-			free(table);
-		}
-
+		free(table-> list);
+		free(table);
 	}
 
-	int table_put(struct table_t *table, char * key, struct data_t *value) {
+}
+
+/* Função para adicionar um par chave-valor na tabela.
+* Os dados de entrada desta função deverão ser copiados.
+* Devolve 0 (ok) ou -1 (out of memory, outros erros)
+*/
+int table_put(struct table_t *table, char * key, struct data_t *value) {
 
 		/* Verificar valores de entrada */
-		if(table == NULL || key == NULL || value == NULL){
-			return -1;
-	 }
-
-		/* Criar entry com par chave/valor */
-		struct entry_t* e;
-		e = entry_create(key, value);
-		if(e == NULL){
-			return -1;
-		}
-
-		/* Executar hash para determinar onde inserir a entry na tabela */
-		int hashCode = key_hash(key, table->size);
-		struct list_t* list = table->list[hashCode];
-		if(list == NULL){
-			list = list_create();
-			if(list == NULL) return -1;
-			table->list[hashCode] = list;
-		}
-		/* Inserir entry na tabela */
-		int retVal = list_add(list,e);
-		if(retVal == 0){
-			table->size++;
-			table->nElem++;
-			return 0;
-		}
-		else
+	if(table == NULL || key == NULL || value == NULL){
 		return -1;
 	}
 
-	int table_update(struct table_t *table, char * key, struct data_t *value) {
-		/* Verificar valores de entrada */
-		if(table == NULL || key == NULL || value == NULL) return -1;
-
 		/* Criar entry com par chave/valor */
-		struct entry_t* e = entry_create(key, value);
-		if(e == NULL) return -1;
-
-		/* Executar hash para determinar onde inserir a entry na tabela */
-		int hashCode = key_hash(key, table->size);
-		struct list_t* list = table->list[hashCode];
-		/* Inserir entry na tabela */
-		int retVal = list_add(list,e);
-		if (retVal == 0) {
-			return 0;
-		}else{
-			return 1;
-		}
-	}
-
-	struct data_t *table_get(struct table_t *table, char * key){
-		if(table == NULL || key == NULL) return NULL;
-		int hashCode = key_hash(key, table->size);
-		struct list_t* list = table->list[hashCode];
-		struct entry_t * entry;
-		entry = entry_dup(list_get(list, key));
-
-		if(entry == NULL) return NULL;
-		struct data_t* ret_data;
-		ret_data = data_create2(entry->value->datasize,entry->value);
-		if(ret_data == NULL) return NULL;
-
-		return ret_data;
-	}
-
-	int table_del(struct table_t *table, char *key){
-		if(table == NULL || key == NULL) return -1;
-
-		int hashCode = key_hash(key, table->size);
-		struct list_t* list = table->list[hashCode];
-		int retVal = list_remove(list, key);
-		if(retVal == 0){
-			table->nElem--;
-			return 0;
-		}
-		else
+	struct entry_t* e;
+	e = entry_create(key, value);
+	if(e == NULL){
 		return -1;
 	}
 
-	/* Esta é dada! Ao estilo C! */
-	int table_size(struct table_t *table) {
+		/* Executar hash para determinar onde inserir a entry na tabela */
+	int hashCode = key_hash(key, table->size);
+	struct list_t* list = table->list[hashCode];
+	if(list == NULL){
+		list = list_create();
+		if(list == NULL) return -1;
+		table->list[hashCode] = list;
+	}
+		/* Inserir entry na tabela */
+	int retVal = list_add(list,e);
+	if(retVal == 0){
+		table->size++;
+		table->nElem++;
+		return 0;
+	}
+	else
+		return -1;
+}
+
+/* Função para substituir na tabela, o valor associado à chave key.
+* Os dados de entrada desta função deverão ser copiados.
+* Devolve 0 (OK) ou -1 (out of memory, outros erros)
+*/
+int table_update(struct table_t *table, char * key, struct data_t *value) {
+		/* Verificar valores de entrada */
+	if(table == NULL || key == NULL || value == NULL) return -1;
+
+		/* Criar entry com par chave/valor */
+	struct entry_t* e = entry_create(key, value);
+	if(e == NULL) return -1;
+
+		/* Executar hash para determinar onde inserir a entry na tabela */
+	int hashCode = key_hash(key, table->size);
+	struct list_t* list = table->list[hashCode];
+		/* Inserir entry na tabela */
+	int retVal = list_add(list,e);
+	if (retVal == 0) {
+		return 0;
+	}else{
+		return 1;
+	}
+}
+
+/* Função para obter da tabela o valor associado à chave key.
+* A função deve devolver uma cópia dos dados que terão de
+* ser libertados no contexto da função que chamou table_get.
+* Devolve NULL em caso de erro.
+*/
+struct data_t *table_get(struct table_t *table, char * key){
+	if(table == NULL || key == NULL) return NULL;
+	int hashCode = key_hash(key, table->size);
+	struct list_t* list = table->list[hashCode];
+	struct entry_t * entry;
+	entry = entry_dup(list_get(list, key));
+
+	if(entry == NULL) return NULL;
+	struct data_t* ret_data;
+	ret_data = data_create2(entry->value->datasize,entry->value);
+	if(ret_data == NULL) return NULL;
+
+	return ret_data;
+}
+
+/* Função para remover um par chave valor da tabela, especificado
+* pela chave key, libertando a memória associada a esse par.
+* Devolve: 0 (OK), -1 (nenhum tuplo encontrado; outros erros)
+*/
+int table_del(struct table_t *table, char *key){
+	if(table == NULL || key == NULL) return -1;
+
+	int hashCode = key_hash(key, table->size);
+	struct list_t* list = table->list[hashCode];
+	int retVal = list_remove(list, key);
+	if(retVal == 0){
+		table->nElem--;
+		return 0;
+	}
+	else
+		return -1;
+}
+
+/* Devolve o número de elementos na tabela.
+*/
+int table_size(struct table_t *table) {
 		/*ATENCAO QUE NAO PODE SER O SIZE (PEDRO)*/
-		return table == NULL ? -1 : table -> nElem;
-	}
+	return table == NULL ? -1 : table -> nElem;
+}
 
-	char **table_get_keys(struct table_t *table) {
-		if (table == NULL) return NULL;
+/* Devolve um array de char * com a cópia de todas as keys da
+* tabela, e um último elemento a NULL.
+*/
+char **table_get_keys(struct table_t *table) {
+	if (table == NULL) return NULL;
 		struct list_t** current_list = NULL; // como se percorre as listas na tabela? ~ miguel
 		char** table_keys_list;
 		table_keys_list = malloc( table_size(table) * sizeof(char**));
@@ -185,6 +211,8 @@ int key_hash(char *key, int l){
 		return table_keys_list;
 	}
 
-	void table_free_keys(char **keys) {
+/* Liberta a memória alocada por table_get_keys().
+*/
+void table_free_keys(char **keys) {
 		list_free_keys(keys);
-	}
+}
