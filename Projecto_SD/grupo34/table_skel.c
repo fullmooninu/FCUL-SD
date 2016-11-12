d2ef7f53276bbc8c92e4738949fae94694300455
pedropais <pedropais@localhost>
2016-11-12 18:43:14 +0000

    table_skel.c atualizado

--- Projecto_SD/grupo34/table_skel.c
+++ Projecto_SD/grupo34/table_skel.c
@@ -7,13 +7,14 @@
 #include "inet.h"
 #include "table-private.h"
 #include "table.h"
+#include "data.h"
 
 struct table_t *table;
 
 int table_skel_init(int n_lists) {
 	table = table_create(n_lists);
 
-	if (table == NULL || table == errno)
+	if (table == NULL)
 		return -1;
 	else
 		return 0;
@@ -22,65 +23,70 @@ int table_skel_init(int n_lists) {
 
 int table_skel_destroy() {
 	table_destroy(table);
+	return 0;
 }
 
 struct message_t *invoke(struct message_t *msg_in) {
 	int aux;
 
 	if (msg_in == NULL)
-		return -1;
+		return NULL;
 
 	switch (msg_in->opcode) {
-	case OC_SIZE {
+	case OC_SIZE:
 		aux = table_size(table);
 		msg_in->content.result = aux;
 		msg_in->opcode = OC_SIZE + 1;
 		msg_in->c_type = CT_RESULT;
-	}break;
+		break;
 
-	case OC_DEL {
+	case OC_DEL:
 		aux = table_del(table, msg_in->content.key);
 		msg_in->opcode = OC_DEL + 1;
 		msg_in->c_type = CT_RESULT;
 
-		if((msg_in->content.result = aux)!= 0) return -1;
+		if ((msg_in->content.result = aux) != 0)
+			return NULL;
 
-	}break;
+		break;
 
-	case OC_UPDATE {
-		aux = table_update(table, msg_in->content.key, msg_in->content.value);
+	case OC_UPDATE:
+		aux = table_update(table, msg_in->content.key, msg_in->content.data);
 		msg_in->opcode = OC_UPDATE + 1;
 		msg_in->c_type = CT_RESULT;
 
-		if((msg_in->content.result = aux) != 0) return -1;
-	}break;
+		if ((msg_in->content.result = aux) != 0)
+			return NULL;
+		break;
 
-	case OC_GET {
+		case OC_GET :
 
 		msg_in->opcode = OC_GET + 1;
 		msg_in->c_type = CT_VALUE;
-		msg_in-> content.value = table_get(table, msg->content.key);
+		msg_in->content.data = table_get(table, msg_in->content.key);
 
 		//cria mensagem de resposta
-		if(msg_in->content.value == NULL)
-		msg_in->content.value= datacreate2(0,NULL);
+		if (msg_in->content.data == NULL)
+			msg_in->content.data = data_create2(0, NULL);
 
-	}break;
+		break;
 
-	case OC_PUT {
+		case OC_PUT :
 
 		msg_in->opcode = OC_PUT + 1;
 		msg_in->c_type = CT_RESULT;
-		aux = table_put(table, msg_in->content.entry->key, msg_in->content.entry->value);
+		aux = table_put(table, msg_in->content.entry->key,
+				msg_in->content.entry->value);
 
-		if((msg_in->content.result = aux) != 0) return -1;
-	}break;
+		if ((msg_in->content.result = aux) != 0)
+			return NULL;
+		break;
 
-	default {
+	default:
 		printf("Mensagem recebida com OP_CODE inválido\n");
-		return -1;
+		return NULL;
+
+		break;
 	}
-	break;
-}
-return msg_in;
+	return msg_in;
 }
