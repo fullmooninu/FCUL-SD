a9d3141bbfe9ede305f04b799127c86dc0941e6c
Aqua1986 <ptpais86@gmail.com>
2016-12-02 22:47:56 +0000

    Adicionada a tolerância a faltas definido no enunciado n3 a client_stub.c

--- proj3-Pedro/grupo34/client_stub.c
+++ proj3-Pedro/grupo34/client_stub.c
@@ -1,27 +1,29 @@
 /* Sistemas Distribuidos - 2016 - Grupo 34
-Elias Miguel Barreira 40821, Pedro Pais 41375
-Silvia Ferreira 45511 */
+ Elias Miguel Barreira 40821, Pedro Pais 41375
+ Silvia Ferreira 45511 */
 
 #include <stdio.h>
 #include <errno.h>
 #include "client_stub.h"
 #include "client_stub-private.h"
+#include "network_client-private.h"
 
 //
-struct rtable_t *rtable_bind(const char *address_port){
-	struct rtable_t *rtable = (struct rtable_t*) malloc(sizeof(struct rtable_t));
+struct rtable_t *rtable_bind(const char *address_port) {
+	struct rtable_t *rtable = (struct rtable_t*) malloc(
+			sizeof(struct rtable_t));
 
 	rtable->server = network_connect(address_port);
 
 	return rtable;
 }
 
-int rtable_unbind(struct rtable_t *rtable){
-	if (network_close(rtable->server) == -1) return -1;
-  free(rtable);
+int rtable_unbind(struct rtable_t *rtable) {
+	if (network_close(rtable->server) == -1)
+		return -1;
+	free(rtable);
 	return 0;
 }
-
 
 int rtable_put(struct rtable_t *rtable, char *key, struct data_t *data) {
 	struct message_t *msg_out, *msg_resposta;
@@ -29,31 +31,35 @@ int rtable_put(struct rtable_t *rtable, char *key, struct data_t *data) {
 	int result = -1;
 
 	msg_out = (struct message_t *) malloc(sizeof(struct message_t));
-  if (msg_out == NULL) return -1;
+	if (msg_out == NULL)
+		return -1;
 
-  msg_out->c_type = CT_ENTRY;
+	msg_out->c_type = CT_ENTRY;
 
-  datat = data_create2(data->datasize, data->data);
-  if (datat == NULL) {
-    free_message(msg_out);
-    return -1;
-  }
-  msg_out->content.entry = entry_create(key, datat);
-  if (msg_out->content.entry == NULL) {
+	datat = data_create2(data->datasize, data->data);
+	if (datat == NULL) {
+		free_message(msg_out);
+		return -1;
+	}
+	msg_out->content.entry = entry_create(key, datat);
+	if (msg_out->content.entry == NULL) {
 		data_destroy(datat);
-    free_message(msg_out);
-    return -1;
-  }
+		free_message(msg_out);
+		return -1;
+	}
 
 	msg_out->opcode = OC_PUT;
 
 	msg_resposta = network_send_receive(rtable->server, msg_out);
 
-
 	if (msg_resposta->opcode == OC_RT_ERROR) {
-		result = -1;
-	} else if (msg_resposta->opcode == OC_PUT+1 &&
-						 msg_resposta->c_type == CT_RESULT) {
+		//Tolerância a falta definida no enunciado Projeto 3
+		sleep(RETRY_TIME);
+		msg_resposta = network_send_receive(rtable->server, msg_out);
+		if (msg_resposta->opcode == OC_RT_ERROR)
+			result = -1;
+	} else if (msg_resposta->opcode == OC_PUT + 1&&
+	msg_resposta->c_type == CT_RESULT) {
 		result = msg_resposta->content.result;
 	}
 
@@ -63,7 +69,6 @@ int rtable_put(struct rtable_t *rtable, char *key, struct data_t *data) {
 
 	return result;
 }
-
 
 int rtable_update(struct rtable_t *rtable, char *key, struct data_t *data) {
 	struct message_t *msg_out, *msg_resposta;
@@ -71,31 +76,35 @@ int rtable_update(struct rtable_t *rtable, char *key, struct data_t *data) {
 	int result = -1;
 
 	msg_out = (struct message_t *) malloc(sizeof(struct message_t));
-  if (msg_out == NULL) return -1;
+	if (msg_out == NULL)
+		return -1;
 
-  msg_out->c_type = CT_ENTRY;
+	msg_out->c_type = CT_ENTRY;
 
-  datat = data_create2(data->datasize, data->data);
-  if (datat == NULL) {
-    free_message(msg_out);
-    return -1;
-  }
-  msg_out->content.entry = entry_create(key, datat);
-  if (msg_out->content.entry == NULL) {
+	datat = data_create2(data->datasize, data->data);
+	if (datat == NULL) {
+		free_message(msg_out);
+		return -1;
+	}
+	msg_out->content.entry = entry_create(key, datat);
+	if (msg_out->content.entry == NULL) {
 		data_destroy(datat);
-    free_message(msg_out);
-    return -1;
-  }
+		free_message(msg_out);
+		return -1;
+	}
 
 	msg_out->opcode = OC_UPDATE;
 
 	msg_resposta = network_send_receive(rtable->server, msg_out);
 
-
 	if (msg_resposta->opcode == OC_RT_ERROR) {
-		result = -1;
-	} else if (msg_resposta->opcode == OC_UPDATE+1 &&
-						 msg_resposta->c_type == CT_RESULT) {
+		//Tolerância a falta definida no enunciado Projeto 3
+		sleep(RETRY_TIME);
+		msg_resposta = network_send_receive(rtable->server, msg_out);
+		if (msg_resposta->opcode == OC_RT_ERROR)
+			result = -1;
+	} else if (msg_resposta->opcode == OC_UPDATE + 1&&
+	msg_resposta->c_type == CT_RESULT) {
 		result = msg_resposta->content.result;
 	}
 
@@ -106,29 +115,33 @@ int rtable_update(struct rtable_t *rtable, char *key, struct data_t *data) {
 	return result;
 }
 
-
 struct data_t *rtable_get(struct rtable_t *table, char *key) {
 	struct message_t *msg_out, *msg_resposta;
 	struct data_t *datat = NULL;
 
 	msg_out = (struct message_t *) malloc(sizeof(struct message_t));
-  if (msg_out == NULL) return NULL;
+	if (msg_out == NULL)
+		return NULL;
 
-  msg_out->c_type = CT_KEY;
-  msg_out->content.key = strdup(key);
-  if (msg_out->content.key == NULL) {
-    free_message(msg_out);
-    return NULL;
-  }
+	msg_out->c_type = CT_KEY;
+	msg_out->content.key = strdup(key);
+	if (msg_out->content.key == NULL) {
+		free_message(msg_out);
+		return NULL;
+	}
 
 	msg_out->opcode = OC_GET;
 
 	msg_resposta = network_send_receive(table->server, msg_out);
 
 	if (msg_resposta->opcode == OC_RT_ERROR) {
-		datat = NULL;
-	} else if (msg_resposta->opcode == OC_GET+1 &&
-						 msg_resposta->c_type == CT_VALUE) {
+		//Tolerância a falta definida no enunciado Projeto 3
+		sleep(RETRY_TIME);
+		msg_resposta = network_send_receive(rtable->server, msg_out);
+		if (msg_resposta->opcode == OC_RT_ERROR)
+			datat = NULL;
+	} else if (msg_resposta->opcode == OC_GET + 1&&
+	msg_resposta->c_type == CT_VALUE) {
 
 		if (msg_resposta->content.data->datasize == 0) {
 			datat = (struct data_t*) malloc(sizeof(struct data_t));
@@ -149,29 +162,33 @@ struct data_t *rtable_get(struct rtable_t *table, char *key) {
 	return datat;
 }
 
-
-int rtable_del(struct rtable_t *table, char *key){
+int rtable_del(struct rtable_t *table, char *key) {
 	struct message_t *msg_out, *msg_resposta;
 	int result = -1;
 
 	msg_out = (struct message_t *) malloc(sizeof(struct message_t));
-  if (msg_out == NULL) return -1;
+	if (msg_out == NULL)
+		return -1;
 
-  msg_out->c_type = CT_KEY;
-  msg_out->content.key = strdup(key);
-  if (msg_out->content.key == NULL) {
-    free_message(msg_out);
-    return -1;
-  }
+	msg_out->c_type = CT_KEY;
+	msg_out->content.key = strdup(key);
+	if (msg_out->content.key == NULL) {
+		free_message(msg_out);
+		return -1;
+	}
 
 	msg_out->opcode = OC_DEL;
 
 	msg_resposta = network_send_receive(table->server, msg_out);
 
 	if (msg_resposta->opcode == OC_RT_ERROR) {
-		result = -1;
-	} else if (msg_resposta->opcode == OC_DEL+1 &&
-						 msg_resposta->c_type == CT_RESULT) {
+		//Tolerância a falta definida no enunciado Projeto 3
+		sleep(RETRY_TIME);
+		msg_resposta = network_send_receive(rtable->server, msg_out);
+		if (msg_resposta->opcode == OC_RT_ERROR)
+			result = -1;
+	} else if (msg_resposta->opcode == OC_DEL + 1&&
+	msg_resposta->c_type == CT_RESULT) {
 		result = msg_resposta->content.result;
 	}
 
@@ -181,13 +198,13 @@ int rtable_del(struct rtable_t *table, char *key){
 	return result;
 }
 
-
-int rtable_size(struct rtable_t *rtable){
+int rtable_size(struct rtable_t *rtable) {
 	struct message_t *msg_out, *msg_resposta;
 	int result = -1;
 
 	msg_out = (struct message_t *) malloc(sizeof(struct message_t));
-	if (msg_out == NULL) return -1;
+	if (msg_out == NULL)
+		return -1;
 
 	msg_out->opcode = OC_SIZE;
 	msg_out->c_type = -1;
@@ -195,9 +212,13 @@ int rtable_size(struct rtable_t *rtable){
 	msg_resposta = network_send_receive(rtable->server, msg_out);
 
 	if (msg_resposta->opcode == OC_RT_ERROR) {
-		result = -1;
-	} else if (msg_resposta->opcode == OC_SIZE+1 &&
-						 msg_resposta->c_type == CT_RESULT) {
+		//Tolerância a falta definida no enunciado Projeto 3
+		sleep(RETRY_TIME);
+		msg_resposta = network_send_receive(rtable->server, msg_out);
+		if (msg_resposta->opcode == OC_RT_ERROR)
+			result = -1;
+	} else if (msg_resposta->opcode == OC_SIZE + 1&&
+	msg_resposta->c_type == CT_RESULT) {
 		result = msg_resposta->content.result;
 	}
 
@@ -207,14 +228,14 @@ int rtable_size(struct rtable_t *rtable){
 	return result;
 }
 
-
-char **rtable_get_keys(struct rtable_t *rtable){
+char **rtable_get_keys(struct rtable_t *rtable) {
 	struct message_t *msg_out, *msg_resposta;
 	char **keys = NULL;
 	int n_keys = 0;
 
 	msg_out = (struct message_t *) malloc(sizeof(struct message_t));
-	if (msg_out == NULL) return NULL;
+	if (msg_out == NULL)
+		return NULL;
 
 	msg_out->c_type = CT_KEY;
 	msg_out->content.key = strdup("*");
@@ -224,9 +245,13 @@ char **rtable_get_keys(struct rtable_t *rtable){
 	msg_resposta = network_send_receive(rtable->server, msg_out);
 
 	if (msg_resposta->opcode == OC_RT_ERROR) {
-		keys = NULL;
-	} else if (msg_resposta->opcode == OC_GET+1 &&
-						 msg_resposta->c_type == CT_KEYS) {
+		//Tolerância a falta definida no enunciado Projeto 3
+		sleep(RETRY_TIME);
+		msg_resposta = network_send_receive(rtable->server, msg_out);
+		if (msg_resposta->opcode == OC_RT_ERROR)
+			keys = NULL;
+	} else if (msg_resposta->opcode == OC_GET + 1&&
+	msg_resposta->c_type == CT_KEYS) {
 		keys = msg_resposta->content.keys;
 		//keys_size
 		while (*keys != NULL) {
